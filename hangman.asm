7da5a1b76f31cf318bfeb406eab24fbd03fd40a1
unknown <hurraira51@gmail.com>
2026-05-17 16:26:05 +0500

    Add Process_Guess logic, hangman procedures, and full game loop integration

--- hangman.asm
+++ hangman.asm
@@ -47,73 +47,70 @@ msgNewline db 13, 10, "$"
 msgEnter   db 13, 10, "Press any key to exit...", "$"
 
 ;Hangman Stages 
-hang0 db '  +---+  ', 13, 10,
-           db '  |   |  ', 13, 10,
-           db '  |      ', 13, 10,
-           db '  |      ', 13, 10,
-           db '  |      ', 13, 10,
-           db '  |      ', 13, 10,
-           db '=========$'
-hang1 db '  +---+  ', 13, 10,
-           db '  |   |  ', 13, 10,
-           db '  |   O  ', 13, 10,
-           db '  |      ', 13, 10,
-           db '  |      ', 13, 10,
-           db '  |      ', 13, 10,
-           db '=========$'
-hang2 db '  +---+  ', 13, 10,
-           db '  |   |  ', 13, 10,
-           db '  |   O  ', 13, 10,
-           db '  |   |  ', 13, 10,
-           db '  |      ', 13, 10,
-           db '  |      ', 13, 10,
-           db '=========$'
+hang0 db "  +---+  ", 13, 10,
+           db "  |   |  ", 13, 10,
+           db "  |      ", 13, 10,
+           db "  |      ", 13, 10,
+           db "  |      ", 13, 10,
+           db "  |      ", 13, 10,
+           db "=========$"
+hang1 db "  +---+  ", 13, 10,
+           db "  |   |  ", 13, 10,
+           db "  |   O  ", 13, 10,
+           db "  |      ", 13, 10,
+           db "  |      ", 13, 10,
+           db "  |      ", 13, 10,
+           db "=========$"
+hang2 db "  +---+  ", 13, 10,
+           db "  |   |  ", 13, 10,
+           db "  |   O  ", 13, 10,
+           db "  |   |  ", 13, 10,
+           db "  |      ", 13, 10,
+           db "  |      ", 13, 10,
+           db "=========$"
 hang3 db '  +---+  ', 13, 10,
-           db '  |   |  ', 13, 10,
-           db '  |   O  ', 13, 10,
-           db '  |  /|  ', 13, 10,
-           db '  |      ', 13, 10,
-           db '  |      ', 13, 10,
-           db '=========$'
-hang4 db '  +---+  ', 13, 10,
-           db '  |   |  ', 13, 10,
-           db '  |   O  ', 13, 10,
-           db '  |  /|\ ', 13, 10,
-           db '  |      ', 13, 10,
-           db '  |      ', 13, 10,
-           db '=========$'
-hang5 db '  +---+  ', 13, 10,
-           db '  |   |  ', 13, 10,
-           db '  |   O  ', 13, 10,
-           db '  |  /|\ ', 13, 10,
-           db '  |  /   ', 13, 10,
-           db '  |      ', 13, 10,
-           db '=========$'
-hang6 db '  +---+  ', 13, 10,
-           db '  |   |  ', 13, 10,
-           db '  |   O  ', 13, 10,
-           db '  |  /|\ ', 13, 10,
-           db '  |  / \ ', 13, 10,
-           db '  |      ', 13, 10,
-           db '=========$'
+           db "  |   |  ", 13, 10,
+           db "  |   O  ", 13, 10,
+           db "  |  /|  ", 13, 10,
+           db "  |      ", 13, 10,
+           db "  |      ", 13, 10,
+           db "=========$"
+hang4 db "  +---+  ", 13, 10,
+           db "  |   |  ", 13, 10,
+           db "  |   O  ", 13, 10,
+           db "  |  /|\ ", 13, 10,
+           db "  |      ", 13, 10,
+           db "  |      ", 13, 10,
+           db "=========$"
+hang5 db "  +---+  ", 13, 10,
+           db "  |   |  ", 13, 10,
+           db "  |   O  ", 13, 10,
+           db "  |  /|\ ", 13, 10,
+           db "  |  /   ", 13, 10,
+           db "  |      ", 13, 10,
+           db "=========$"
+hang6 db "  +---+  ", 13, 10,
+           db "  |   |  ", 13, 10,
+           db "  |   O  ", 13, 10,
+           db "  |  /|\ ", 13, 10,
+           db "  |  / \ ", 13, 10,
+           db "  |      ", 13, 10,
+           db "=========$"
 hangPtrs dw offset hang0, offset hang1, offset hang2, offset hang3, 
-dw offset hang4, offset hang5, offset hang6
+         dw offset hang4, offset hang5, offset hang6
 
 .code
 main proc
 
        mov ax, @data
        mov ds, ax
-
        mov ah, 00h
        int 1ah
 
 
        xor dx, cx
        mov randSeed, dx
-
        call Pick_Word
-
 
 
 Game_Loop:
@@ -261,12 +258,12 @@ lea si, guessedWord
 
 pg_loop:
 mov al, [si]
-cmp al, '$'
+cmp al, "$"
 je pg_done 
 mov dl, al 
 mov ah, 02h
 int 21h 
-mov dl, ' '
+mov dl, " "
 mov ah, 02h
 int 21h 
 inc si 
@@ -292,7 +289,10 @@ mov al, [si]
 cmp al, 0 
 je pw_done
 mov dl, al 
-mov ah, 02h
+mov ah, 02h 
+int 21h 
+mov dl, " "
+mov ah,02h
 int 21h
 inc si 
 loop pw_loop 
@@ -301,6 +301,7 @@ pw_done:
 pop si 
 pop dx
 pop cx
+pop ax
 ret 
 Print_Wrong_Letters endp 
 
@@ -312,13 +313,13 @@ lea si, currentWord
 
 pcw_loop:
 mov al, [si]
-cmp al, '$'
+cmp al, "$"
 je pcw_done 
 mov dl, al 
 mov ah, 02h
 int 21h 
 inc si 
-imp pcw_loop 
+jmp pcw_loop 
 
 pcw_done:
 pop si 
@@ -347,7 +348,128 @@ pop cx
 pop bx
 pop ax 
 ret 
-Clear_Screen endp
+Clear_Screen endp 
+
+Process_Guess proc
+    
+    push ax
+    push cx
+    push si
+    push di
+    
+    lea si, wrongGuesses 
+    mov cx, 20
+
+Check_Wrong_dup:         
+
+    mov al, [si]
+    cmp al, 0
+    je Not_In_Wrong
+    cmp al, bl
+    je Already_guessed
+    inc si
+    Loop Check_Wrong_dup
+    
+Not_In_Wrong:
+
+    lea si, guessedWord
+    
+Check_Correct_dup:  
+
+    mov al, [si]
+    cmp al, "$"
+    je Not_In_Correct
+    cmp al, bl
+    je Already_Guessed
+    inc si
+    jmp Check_Correct_dup
+    
+Not_In_Correct:
+
+    lea si, currentWord
+    lea di, guessedWord
+    mov cx, 0
+    
+Search_Loop: 
+
+    mov al, [si]
+    cmp al, "$"
+    je Search_Done
+    cmp al, bl
+    jne No_Match
+    mov [di], bl
+    inc cx
+    inc guessedCount
+    
+No_Match: 
+
+    inc si
+    inc di
+    jmp Search_Loop
+    
+Search_Done:
+    
+    cmp cx, 0
+    je Wrong_Guess
+    lea dx, msgCorrect
+    mov ah, 09h
+    int 21h
+    mov al, guessedCount
+    cmp al, wordLen
+    jge Player_Wins
+    jmp Guess_Done
+    
+Wrong_Guess:
+
+    lea di, wrongGuesses
+    mov cx, 20
+    
+Find_Slot:
+
+    Mov al, [di]
+    cmp al, 0
+    je Add_Wrong
+    inc di
+    Loop Find_Slot 
+    jmp Guess_Done 
+    
+Add_Wrong:
+    
+    mov [di], bl
+    inc wrongCount
+    lea dx, msgWrong
+    mov ah, 09h
+    int 21h
+    cmp wrongCount, 6
+    jge Player_Loses
+    jmp Guess_Done
+    
+Already_Guessed:
+
+   lea dx, msgAlready
+   mov ah, 09h
+   int 21h
+   jmp Guess_Done
+   
+Player_Wins:
+
+   mov gameOver, 1
+   jmp Guess_Done
+   
+Player_Loses:
+
+   mov gameOver, 2
+   
+Guess_Done:
+
+   pop di
+   pop si
+   pop cx
+   pop ax
+   ret
+   
+Process_Guess endp
+    
 
 
 end main
